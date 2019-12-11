d77be3c50eb1be0cdd28e227af30f7206ffe973f
blaueeiner <blaueeiner@gmail.com>
2019-12-11 19:45:47 +0100

    Prepared v1.0.1 release

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { UtilsService } from "../../core/services/utils/utils.service";
+import { version } from "../../../../package.json";
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './news.component.html',
-  styleUrls: ['./news.component.scss']
+  selector: "app-news",
+  templateUrl: "./news.component.html",
+  styleUrls: ["./news.component.scss"]
 })
 export class NewsComponent implements OnInit {
+  public version: string = version;
 
-  constructor() { }
+  constructor(public _utilsService: UtilsService) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
